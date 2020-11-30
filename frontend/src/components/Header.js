c6a6fa3a7334aad1fd02e02b76bfc83d13620af7
Theelipan prabakar <theelipan@gmail.com>
2020-11-30 01:21:34 -0500

    cart items counter label in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Dropdown } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
+import { Dropdown, Label, Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const cartItemsNum = String(cartItems.reduce((a, { qty }) => a + qty, 0));
+  console.log('🚀 ~ file: Header.js ~ line 10 ~ Header ~ cartItemsNum', cartItemsNum);
+
   return (
     <header>
       <div className='nav'>
@@ -11,6 +17,7 @@ const Header = () => {
             <img src='/images/icons/logo.svg' alt='' />
           </Link>
         </div>
+
         <div className='nav__search'>
           <button type='submit'>
             <img src='/images/icons/search.svg' alt='' />
@@ -18,11 +25,6 @@ const Header = () => {
           <input type='text' placeholder='Search'></input>
         </div>
 
-        <div className='nav__cart'>
-          <Link to='/cart'>
-            <img src='/images/icons/cart.svg' alt='' />
-          </Link>
-        </div>
         <div className='nav__dropdown'>
           <Dropdown pointing='top right' icon={<img src='/images/icons/user.svg' alt='' className='ui avatar image' />}>
             <Dropdown.Menu style={{ boxShadow: 'none', padding: '15px', borderRadius: '0' }}>
@@ -34,6 +36,23 @@ const Header = () => {
               </Link>
             </Dropdown.Menu>
           </Dropdown>
+        </div>
+
+        <div className='nav__cart'>
+          <Menu compact>
+            <Menu.Item>
+              <Link to='/cart'>
+                <img src='/images/icons/cart.svg' alt='' />
+              </Link>
+              {cartItemsNum !== '0' ? (
+                <Label color='orange' circular floating>
+                  {cartItemsNum}
+                </Label>
+              ) : (
+                false
+              )}
+            </Menu.Item>
+          </Menu>
         </div>
       </div>
     </header>
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Button, Container, Form } from 'semantic-ui-react';
+import { Button, Container, Divider, Form } from 'semantic-ui-react';
 
 const LoginScreen = () => {
   const style = {
-    margin: '5rem auto',
+    margin: '5rem auto 4rem',
     maxWidth: '350px',
   };
   return (
-    <Container>
-      <Form style={style}>
-        <Form.Input icon='user' iconPosition='left' label='Username' placeholder='Username' />
-        <Form.Input icon='lock' iconPosition='left' label='Password' type='password' />
-        <Button color='black' type='submit' style={{ width: '100%' }}>
-          Submit
-        </Button>
-      </Form>
-    </Container>
+    <>
+      <Container>
+        <Form style={style}>
+          <Form.Input size='large' icon='user' iconPosition='left' label='Email Address' placeholder='Email Address' style={{ textWeight: 'normal' }} />
+          <Form.Input size='large' icon='lock' iconPosition='left' label='Password' type='password' placeholder='Password' />
+          <Button color='black' type='submit' style={{ width: '100%' }}>
+            Submit
+          </Button>
+        </Form>
+        <Divider horizontal>New Customer?</Divider>
+      </Container>
+      <Container textAlign='center'>
+        <div style={style}>
+          <Button type='submit' style={{ width: '100%' }}>
+            Register
+          </Button>
+        </div>
+      </Container>
+    </>
   );
 };
 
