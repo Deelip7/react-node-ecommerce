c5f4cebdd04ffc779a4fca603f7acce0d1de1a7c
Theelipan prabakar <theelipan@gmail.com>
2020-12-23 00:32:30 -0500

    fix bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -62,7 +62,9 @@ const Header = ({ history }) => {
           <Menu compact>
             <Menu.Item>
               <Link to='/cart'>
-                <img src='/images/icons/cart.svg' alt='' />
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='#656565' width='2.19rem'>
+                  <path stroke-linecap='round' stroke-linejoin='round' stroke-width='1' d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z' />
+                </svg>
               </Link>
               {cartItemsNum !== '0' ? (
                 <Label color='orange' circular floating className='cart-lable'>
